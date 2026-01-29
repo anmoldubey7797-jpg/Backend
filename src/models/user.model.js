e3e50f3190848642fb75f58df5252e6869dcfabe
Anmol Dubey <anmoldubey7797@gmail.com>
2026-01-29 12:07:42 +0530

    changes allowed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema=new Schema(
     timestamps:true
 })
 
-userSchema.pre("save", async function(next) {
+userSchema.pre("save", async function() {
 
     if(!this.isModified("password")) return next();
 
